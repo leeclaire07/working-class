547f5dc1b47c56c8c3591d586eec410abac24165
leeclaire07 <somii5183@gmail.com>
2024-07-24 13:43:41 +0900

    240724걷기반 실습

---
+++ 240724/03.js
@@ -0,0 +1,3 @@
+let str3 ="learning javascript is fun!";
+let result01=str3.replace("javascript","coding");
+console.log(result01);
---
+++ 240724/04.js
@@ -0,0 +1,12 @@
+const lion = {
+    이름 : "심바",
+    나이 : 3,
+    주인공 :true,
+    대사 :"하쿠나마타타",
+};
+function incrementAge(lion) {
+   lion.age += 1;
+
+   return lion.age;
+};
+console.log(incrementAge(lion))
